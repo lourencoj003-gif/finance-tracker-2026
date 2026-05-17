001d561f4429f13e8cbcb89141a2951a00b4d0fe
Lourenco Jeronimo <lourencojeronimo@Lourencos-MacBook-Air.local>
2026-05-17 02:29:57 +0100

    Vela JARVIS rebuild complete

--- src/utils/helpers.js
+++
@@ -1,5 +0,0 @@
-export const MONTHS = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-
-export function fmt(n) {
-  return "£" + Math.abs(Math.round(n)).toLocaleString("en-GB");
-}
